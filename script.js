2e1cd85b9b7c3c45c4efd7d548397cdd3e59397a
MahdiMohammadi98125 <109587937+MahdiMohammadi98125@users.noreply.github.com>
2022-12-24 17:10:55 -0800

    the functionality of reset button added

--- script.js
+++ script.js
@@ -11,14 +11,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let currentScore = 0;
-const scores = [0, 0];
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -74,6 +87,4 @@ btnHold.addEventListener('click', function () {
 });
 
 // RESETTING THE GAME
-btnNew.addEventListener('click', function () {
-  console.log('called');
-});
+btnNew.addEventListener('click', init);
